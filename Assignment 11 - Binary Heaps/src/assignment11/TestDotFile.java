bca54e2a5e9605b4b416bff9afe2c5d35d48119a
Nathan <Nathan@10.0.1.8>
2016-04-05 09:29:11 -0600

    Added Java Class to create a dot file.

---
+++ Assignment 11 - Binary Heaps/src/assignment11/TestDotFile.java
@@ -0,0 +1,27 @@
+package assignment11;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class TestDotFile {
+	
+	public static void main(String[] args){
+	
+	
+	
+	ArrayList<Integer> smallIntArray = new ArrayList<>();
+	for (int i = 0; i < 100; i++) {
+		smallIntArray.add(i);
+	}
+	Collections.shuffle(smallIntArray);
+	
+	PriorityQueue pQ_Int = new PriorityQueue<Integer>();
+	
+	for (int element : smallIntArray) {
+		pQ_Int.add(element);
+	}
+	
+	pQ_Int.generateDotFile("assignment11DOT.dot");
+
+	}
+}
